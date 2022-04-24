83fd5d48f281a384c3d60305b760708d6a6e4e61
Eduardo Herrera <Eduardo@7seeds.cl>
2022-04-24 18:17:06 -0400

    Initial Commit

---
+++ Shared/Bridge_Helper_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Bridge_Helper_2022App.swift
+//  Shared
+//
+//  Created by Eduardo Herrera Barros on 24-04-22.
+//
+
+import SwiftUI
+
+@main
+struct Bridge_Helper_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
